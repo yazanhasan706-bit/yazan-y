a5d02c35a368836f1ffc204def6e780635680fe7
yazanhasan706-bit <yazanhasan706@gmail.com>
2026-08-26 22:05:45 +0300

    Create app/build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.yazan.translator"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.yazan.translator"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+}
