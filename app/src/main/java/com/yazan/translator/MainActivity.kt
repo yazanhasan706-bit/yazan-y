a8b43701dae59f06eb724923e90faabeaae830bf
Yazan <yazanhasan706@gmail.com>
2026-08-27 00:43:32 +0000

    Fix screen translator

--- app/src/main/java/com/yazan/translator/MainActivity.kt
+++ app/src/main/java/com/yazan/translator/MainActivity.kt
@@ -27,7 +27,9 @@ class MainActivity : AppCompatActivity() {
             getSystemService(MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
 
         translateButton.setOnClickListener {
-            val captureIntent = mediaProjectionManager.createScreenCaptureIntent()
+            val captureIntent =
+                mediaProjectionManager.createScreenCaptureIntent()
+
             startActivityForResult(
                 captureIntent,
                 SCREEN_CAPTURE_REQUEST_CODE
@@ -46,10 +48,10 @@ class MainActivity : AppCompatActivity() {
         if (requestCode == SCREEN_CAPTURE_REQUEST_CODE) {
             if (resultCode == Activity.RESULT_OK && data != null) {
                 findViewById<TextView>(R.id.resultText).text =
-                    "تم السماح بالتقاط الشاشة!"
+                    "تم السماح بالتقاط الشاشة"
             } else {
                 findViewById<TextView>(R.id.resultText).text =
-                    "لم يتم السماح بالتقاط الشاشة."
+                    "لم يتم السماح بالتقاط الشاشة"
             }
         }
     }
